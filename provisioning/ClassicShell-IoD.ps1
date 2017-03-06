ed32ef56052e63671d30196c8bc6326ef498553e
Javier Jaramago Fernández <jaramago.fernandez.javier@gmail.com>
2017-03-06 17:50:43 +0100

    First version of script for installing Classic Shell on demand

---
+++ provisioning/ClassicShell-IoD.ps1
@@ -0,0 +1,45 @@
+<#
+    This script installs the needed dependencies for the Windows 7 Mopher
+#>
+
+Import-Module (Join-Path (Split-Path -Parent $MyInvocation.MyCommand.Path) 'Provisioning.psm1') -Force
+
+$VerbosePreference = "continue"
+
+$chocolatey = "$env:ChocolateyInstall\bin\choco.exe" -f $env:SystemDrive
+$classicShellVers = "4.3.0.0"
+$classicShellPath = "C:\Program Files\Classic Shell\ClassicStartMenu.exe"
+$classicShellProcessName = "ClassicStartMenu"
+
+try
+{
+    <# Classic-Shell installation #>
+    Invoke-Command $chocolatey "install classic-shell --version $($classicShellVers) -y" "" 0
+}
+catch
+{
+    <# We should notify that IoD have fail #>
+    $ErrorMessage = $_.Except0on.Message
+    Write-Verbose "$ErrorMessage"
+}
+
+try
+{
+    <# Currently there is no way of being sure classicShell has exit #>
+    $process = Get-Process $ClassicShellProcessName -ea SilentlyContinue
+
+    if ($process)
+    {
+        Invoke-Command $classicShellPath "-exit" "" 0
+        Write-Verbose "Classic-Shell stopped"
+    }
+}
+catch
+{
+    <# ($_.FullyQualifiedErrorId).split(',')[0] #>
+    Write-Verbose ("Failed to restore correct Windows Menu " + $_.FullyQualifiedErrorId)
+}
+
+refreshenv
+
+exit 0
